e01a198ff512ab60f76620c33fc9824c9fa6feee
Neilai <1131894367@qq.com>
2019-02-26 22:18:11 +0800

    add img

--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,6 @@
 function exportGPA(data){
     var doc = new jsPDF()
     data.styles={font:"yahei",fontStyle:"normal"}
-    doc.addFont('yahei.ttf', 'custom', 'normal');
-    doc.setFont('custom');
-    data.theme="plain"
-    doc.text(15, 15, '测试');
     doc.autoTable(data);
     doc.save('table.pdf');
 }
